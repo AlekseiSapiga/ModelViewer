0cc855f8e578f5ac0c9a9e683cf0a5398cd508ab
Сапига Алексей <sapigaall1@gmail.com>
2021-06-08 14:03:37 +0300

    Добавил кастомный редактор для выбора типа инвентаря.

---
+++ ModelViewer/Assets/Editor/InventoryCategoryIdProperty.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+
+[CustomPropertyDrawer(typeof(InventoryCategoryId))]
+public class InventoryCategoryIdProperty : PropertyDrawer
+{    
+    private static string[] options = null;
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {        
+        if (options == null)
+        {
+            string[] guids1 = AssetDatabase.FindAssets("InventoryCategoryList t:InventoryCategoryList", null);
+            
+            if (guids1.Length >= 1)
+            {
+                var settings = (InventoryCategoryList)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guids1[0]), typeof(InventoryCategoryList));
+                if (settings != null)
+                {
+                    options = settings._categories;
+                }
+            }
+        }
+        if (options == null)
+        {
+            Debug.LogError("File not found InventoryCategoryList");
+            return;
+        }
+        
+        var propertyId = property.FindPropertyRelative("_id");
+        if (propertyId == null)
+        {
+            Debug.LogError("Didn't find subproperty _id");
+            return;            
+        }
+
+        int index = Mathf.Max(0, Array.IndexOf(options, propertyId.stringValue));
+
+        index = EditorGUI.Popup(position, label.text, index, options);
+
+        propertyId.stringValue = options[index];
+    }
+
+}
---
+++ ModelViewer/Assets/Editor/InventoryCategoryList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryCategoryList", menuName = "ScriptableObjects/InventoryCategoryList", order = 1)]
+public class InventoryCategoryList : ScriptableObject
+{
+    public string[] _categories;
+}
